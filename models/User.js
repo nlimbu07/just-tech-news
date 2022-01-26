f900cc57da0dffca07b2a48c1ca946761ea4b49f
Niraj Limbu <nlimbu@yahoo.com>
2022-01-26 16:51:52 -0500

    add security and authentication

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up medthod to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -60,6 +65,7 @@ User.init(
         return updatedUserData;
       },
     },
+
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
